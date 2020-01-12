1723b66c8b4790830c6f17ede0dae771f6fc3289
Harshad Nawathe <harshadn@thoughtworks.com>
2020-01-12 19:20:49 +0530

    Add extension function description on Pizza

--- src/main/kotlin/com/thoughtworks/workshop/kotlin/pizza/Pizza.kt
+++ src/main/kotlin/com/thoughtworks/workshop/kotlin/pizza/Pizza.kt
@@ -6,3 +6,11 @@ interface Pizza {
     val toppings : List<String>
     val price: Double
 }
+
+fun Pizza.description() : String {
+    val toppingsText = toppings.reduce { acc, topping ->
+        "$acc, $topping"
+    }
+
+    return "A $name pizza with $dough base, topped with $toppingsText and priced at $price"
+}
